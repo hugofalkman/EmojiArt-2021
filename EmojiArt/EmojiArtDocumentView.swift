8c94bfc3b19d695c1e2fd0ac6ff491cfe1118d04
hugofalkman <hugo@falkman.net>
2021-06-16 14:18:47 +0200

    End Lecture 9

---
+++ EmojiArt/EmojiArtDocumentView.swift
@@ -0,0 +1,108 @@
+//
+//  EmojiArtDocumentView.swift
+//  EmojiArt
+//
+//  Created by H Hugo Falkman on 13/06/2021.
+//
+
+import SwiftUI
+
+struct EmojiArtDocumentView: View {
+    @ObservedObject var document: EmojiArtDocument
+    
+    let defaultEmojiFontSize: CGFloat = 40
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            documentBody
+            palette
+        }
+    }
+    
+    var documentBody: some View {
+        GeometryReader { geometry in
+            ZStack {
+                Color.yellow
+                ForEach(document.emojis) { emoji in
+                    Text(emoji.text)
+                        .font(.system(size: fontSize(for: emoji)))
+                        .position(position(for: emoji, in: geometry))
+                }
+            }
+            .onDrop(of: [.plainText], isTargeted: nil) { providers, location in
+                drop(providers: providers, at: location, in: geometry)
+            }
+        }
+    }
+    
+    private func drop(providers: [NSItemProvider], at location: CGPoint, in geometry: GeometryProxy) -> Bool {
+        return providers.loadObjects(ofType: String.self) { string in
+            if let emoji = string.first, emoji.isEmoji {
+                document.addEmoji(
+                    String(emoji),
+                    at: convertToEmojiCoordinates(location, in: geometry),
+                    size: defaultEmojiFontSize
+                )
+            }
+        }
+    }
+    
+    typealias Emoji = EmojiArtModel.Emoji
+    private func position(for emoji: Emoji, in geometry: GeometryProxy ) -> CGPoint {
+        convertFromEmojiCoordinates((emoji.x, emoji.y), in: geometry)
+    }
+    
+    private func convertToEmojiCoordinates(_ location: CGPoint, in geometry: GeometryProxy) -> (x: Int, y: Int) {
+        let center = geometry.frame(in: .local).center
+        let loc = CGPoint(
+            x: location.x - center.x,
+            y: location.y - center.y
+        )
+        return (Int(loc.x), Int(loc.y))
+    }
+    
+    private func convertFromEmojiCoordinates(_ location: (x: Int, y: Int), in geometry: GeometryProxy) -> CGPoint {
+        let center = geometry.frame(in: .local).center
+        return CGPoint(
+            x: center.x + CGFloat(location.x),
+            y: center.y + CGFloat(location.y)
+        )
+    }
+    
+    private func fontSize(for emoji: Emoji) -> CGFloat {
+        CGFloat(emoji.size)
+    }
+    
+    var palette: some View {
+        ScrollingEmojisView(emojis: testEmojis)
+            .font(.system(size: defaultEmojiFontSize))
+    }
+    
+    let testEmojis = "😀😷🦠💉👻👀🐶🌲🌎🌞🔥🍎⚽️🚗🚓🚲🛩🚁🚀🛸🏠⌚️🎁🗝🔐❤️⛔️❌❓✅⚠️🎶➕➖🏳️"
+}
+
+struct ScrollingEmojisView: View {
+    let emojis: String
+    
+    var body: some View {
+        ScrollView(.horizontal) {
+            HStack {
+                ForEach(emojis.map { String($0) }, id: \.self) { emoji in
+                    Text(emoji)
+                        .onDrag { NSItemProvider(object: emoji as NSString) }
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        EmojiArtDocumentView(document: EmojiArtDocument())
+    }
+}
---
+++ EmojiArt/EmojiArtModel.Background.swift
@@ -0,0 +1,31 @@
+//
+//  EmojiArtModel.Background.swift
+//  EmojiArt
+//
+//  Created by H Hugo Falkman on 13/06/2021.
+//
+
+import Foundation
+
+extension EmojiArtModel {
+    
+    enum Background {
+        case blank
+        case url(URL)
+        case imageData(Data)
+        
+        var url: URL? {
+            switch self {
+            case .url(let url): return url
+            default: return nil
+            }
+        }
+        
+        var imageData: Data? {
+            switch self {
+            case .imageData(let data): return data
+            default: return nil
+            }
+        }
+    }
+}
